0287b697820108df8168c688e54921259319b4e8
vdegenne <vdegenne@gmail.com>
2023-03-29 17:04:48 +0200

    delete and ignore build files

--- snar.d.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement } from 'lit';
-export { state } from 'lit/decorators.js';
-export declare function component(clazz: CustomElementConstructor): any;
-export declare class Snar extends LitElement {
-    static styles: import("lit").CSSResult;
-    render(): symbol;
-}
-//# sourceMappingURL=snar.d.ts.map
